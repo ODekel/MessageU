ce177a343cab1ece2084204d53a12faa6e032856
Omer Dekel <omer.dekel722001@gmail.com>
2026-02-24 01:29:59 +0200

    complete request handlers

--- src/server/db.py
+++ src/server/db.py
@@ -40,18 +40,18 @@ def get_client(client_id: UUID) -> Client | None:
     with closing(_conn.cursor()) as curr:
         curr.execute('SELECT * FROM clients WHERE ID = ?', (client_id.bytes, ))
         row = curr.fetchone()
-        if row is None:
-            return None
-        return Client(UUID(bytes=row[0]), row[1], row[2], row[3])
+    if row is None:
+        return None
+    return Client(UUID(bytes=row[0]), row[1], row[2], row[3])
 
 
 def get_client_by_username(username: bytes) -> Client | None:
     with closing(_conn.cursor()) as curr:
         curr.execute('SELECT * FROM clients WHERE UserName = ?', (username, ))
         row = curr.fetchone()
-        if row is None:
-            return None
-        return Client(UUID(bytes=row[0]), row[1], row[2], row[3])
+    if row is None:
+        return None
+    return Client(UUID(bytes=row[0]), row[1], row[2], row[3])
 
 
 def add_client(client: Client) -> None:
@@ -71,25 +71,27 @@ def get_all_clients(exclude: UUID) -> list[Client]:
     with closing(_conn.cursor()) as curr:
         curr.execute('SELECT * FROM clients WHERE ID != ?', (exclude.bytes, ))
         rows = curr.fetchall()
-        return [Client(UUID(bytes=row[0]), row[1], row[2], row[3]) for row in rows]
+    return [Client(UUID(bytes=row[0]), row[1], row[2], row[3]) for row in rows]
 
 
-def add_message(msg: Message) -> None:
+def add_message(msg: Message) -> int:
     with closing(_conn.cursor()) as curr:
         curr.execute('INSERT INTO messages (ToClient, FromClient, Type, Content) VALUES (?, ?, ?, ?)',
                      (msg.to_client.bytes, msg.from_client.bytes, msg.type, msg.content))
+        curr.execute('SELECT last_insert_rowid()')
+        msg_id = curr.fetchone()[0]
         _conn.commit()
+    return msg_id
 
 
 def get_messages_for_client(client_id: UUID) -> list[Message]:
     with closing(_conn.cursor()) as curr:
         curr.execute('SELECT * FROM messages WHERE ToClient = ?', (client_id.bytes, ))
         rows = curr.fetchall()
-        return [Message(row[0], UUID(bytes=row[1]), UUID(bytes=row[2]), row[3], row[4]) for row in rows]
+    return [Message(row[0], UUID(bytes=row[1]), UUID(bytes=row[2]), row[3], row[4]) for row in rows]
 
 
 def delete_messages(message_ids: list[int]) -> None:
     with closing(_conn.cursor()) as curr:
-        curr.execute(f'DELETE FROM messages WHERE ID IN ({", ".join(['?'] * len(message_ids))})',
-                     message_ids)
+        curr.execute(f'DELETE FROM messages WHERE ID IN ({", ".join(['?'] * len(message_ids))})', message_ids)
         _conn.commit()
--- src/server/request_handlers.py
+++ src/server/request_handlers.py
@@ -3,10 +3,10 @@ import struct
 import uuid
 
 import db
-from models import RequestHeaders, ServerException, Client
+from models import RequestHeaders, ServerException, Client, Message
 
 
-def register_client(headers: RequestHeaders, content: bytes) -> bytes:
+def register_client(_: RequestHeaders, content: bytes) -> bytes:
     username, public_key = struct.unpack('!255s160s', content)
     if db.get_client_by_username(username) is not None:
         raise ServerException(f'Client with username "{username}" already exists.')
@@ -15,17 +15,38 @@ def register_client(headers: RequestHeaders, content: bytes) -> bytes:
     return client_id.bytes
 
 
-def get_client_list(headers: RequestHeaders, content: bytes) -> bytes:
-    pass
+def get_client_list(headers: RequestHeaders, _: bytes) -> bytes:
+    clients = db.get_all_clients(headers.client_id)
+    db.update_client_last_seen(headers.client_id)
+    result = b''
+    for client in clients:
+        result += struct.pack('!16s255s', client.client_id.bytes, client.username)
+    return result
 
 
 def get_client_public_key(headers: RequestHeaders, content: bytes) -> bytes:
-    pass
+    client_id = uuid.UUID(bytes=content)
+    client = db.get_client(client_id)
+    db.update_client_last_seen(headers.client_id)
+    if client is None:
+        raise ServerException(f'Client with ID {client_id} not found.')
+    return client_id.bytes + client.public_key
 
 
 def send_message(headers: RequestHeaders, content: bytes) -> bytes:
-    pass
+    to_id, msg_type, msg_content_size = struct.unpack('!16sBI', content[:21])
+    msg_context = content[21:]
+    msg_id = db.add_message(Message(0, uuid.UUID(bytes=to_id), headers.client_id, msg_type, msg_context))
+    db.update_client_last_seen(headers.client_id)
+    return struct.pack('!16sI', to_id, msg_id)
 
 
-def get_messages(headers: RequestHeaders, content: bytes) -> bytes:
-    pass
+def get_messages(headers: RequestHeaders, _: bytes) -> bytes:
+    messages = db.get_messages_for_client(headers.client_id)
+    db.update_client_last_seen(headers.client_id)
+    db.delete_messages([msg.message_id for msg in messages])
+    result = b''
+    for msg in messages:
+        result += (struct.pack('!16sIBI', msg.from_client.bytes, msg.message_id, msg.type, len(msg.content))
+                   + msg.content)
+    return result
